76ef0d71d4ca6225113c19bfdf7d0c8647a17337
Sonal Diwan <80630648+SONA0007@users.noreply.github.com>
2025-01-08 20:45:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,22 @@
 const express = require('express');
-const cors = require('cors'); // Import CORS
+const cors = require('cors');
 const { db } = require('./db/db');
 
-const app = express(); // Initialize the app
-app.use(cors()); // Enable CORS for all routes
+const app = express(); 
+app.use(cors()); 
 const expensesRoutes = require('./routes/expenses');
 const authRoutes = require('./routes/auth');
 const dotenv = require('dotenv');
 const authMiddleware = require('./middleware/authMiddleware');
 
 dotenv.config();
-
-// Middleware to parse JSON bodies
 app.use(express.json()); 
 
-// Connect to the database
 db();
 
-// Define routes
 app.use('/api/expenses', expensesRoutes);
 app.use('/api/auth', authRoutes);
 
-// Start the server
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
