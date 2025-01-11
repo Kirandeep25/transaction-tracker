9189ca83aecc09a7d157c67f3685958542daa56e
Sonal Diwan <80630648+SONA0007@users.noreply.github.com>
2025-01-11 19:02:08 +0530

    Update Login.js

--- front-end/src/components/Auth/Login.js
+++ front-end/src/components/Auth/Login.js
@@ -26,19 +26,21 @@ const Login = ({ onLogin }) => {
     };
 
     return (
-        <div className="container">
-            {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
-            <form onSubmit={handleSubmit}>
-                <h2 className="mt-4">Login</h2>
-                <div className="form-group">
-                    <input type="text" className="form-control" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required />
-                </div>
-                <div className="form-group">
-                    <input type="password" className="form-control" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required />
-                </div>
-                <button type="submit" className="btn btn-primary">Login</button>
-            </form>
+        <div className="container mt-5 d-flex justify-content-center">
+        <div className="card p-4 shadow-lg" style={{ width: '800px' }}>
+          {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+          <form onSubmit={handleSubmit}>
+            <h2 className="text-center mb-4">Login</h2>
+            <div className="form-group mb-3">
+              <input type="text" className="form-control" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+            </div>
+            <div className="form-group mb-3">
+              <input type="password" className="form-control" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} required/>
+            </div>
+            <button type="submit" className="btn btn-primary w-100">Login</button>
+          </form>
         </div>
+      </div>  
     );
 };
 
